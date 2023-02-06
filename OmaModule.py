e86554377efa5bf36b89a5a4793621b382b3c18b
TimurDenisenko <120181244+TimurDenisenko@users.noreply.github.com>
2023-02-06 14:39:48 +0200

    Update OmaModule.py

--- OmaModule.py
+++ OmaModule.py
@@ -86,14 +86,18 @@ def khaiga(x:str)->str:
 
 def karv(x:str)->str:
     xlist=list(map(int,x))
-    arv=1*xlist[0]+2*xlist[1]+3*xlist[2]+4*xlist[3]+5*xlist[4]+6*xlist[5]+7*xlist[6]+8*xlist[7]+9*xlist[8]+1*xlist[9]
+    astme1=[1,2,3,4,5,6,7,8,9,1]
+    astme2=[3,4,5,6,7,8,9,1,2,3]
+    arv=0
+    for i in range(10):
+        arv+=xlist[i]*astme1[i]
     if arv%10==0:
-        arv=3*xlist[0]+4*xlist[1]+5*xlist[2]+6*xlist[3]+7*xlist[4]+8*xlist[5]+9*xlist[6]+1*xlist[7]+2*xlist[8]+3*xlist[9]
-        arv1=arv//11
-        arv=arv-arv1*11
+        arv=0
+        for i in range(10):
+            arv+=xlist[i]*astme2[2]
+        arv=arv-(arv//11)*11
     else:
-        arv1=arv//11
-        arv=arv-arv1*11
+        arv=arv-(arv//11)*11
     if arv==xlist[10]:
         x="Kontrollnumber on korras"
     else:
@@ -102,3 +106,21 @@ def karv(x:str)->str:
 
 def ikood(x: str)->str:
     print(f"See on {ksugu(x)} ta on sündinud {ksupa(x)}, tema sünnikoht on {khaiga(x)}")
+
+def ars(x:list)->list:
+    xlist=list(x)
+    xlist.sort()
+    return xlist
+
+def arg(x:list)->list:
+    naised=[]
+    mehed=[]
+    for kood in x:
+        kood_=list(kood)
+        if int(kood_[0])%2==0:
+            naised.append(kood)
+        else: 
+            mehed.append(kood)
+    naised.extend(mehed)
+    x=naised
+    return x
