bbbfdbb34b5b3ff2c46e06505f3e40c12cdd03e6
TimurDenisenko <120181244+TimurDenisenko@users.noreply.github.com>
2023-02-06 14:40:12 +0200

    Update Isikukood.py

--- Isikukood.py
+++ Isikukood.py
@@ -6,17 +6,20 @@ NoKn=[]
 NoSug=[] 
 NoSun=[] 
 NoHai=[]
+TrueI=[]
 while True:
     isikukood=input("Palun kirjuta sinu isikukood ")
     if iPikk(isikukood)==False:
         print("Isikukood koosneb üheteistkümnest tähemärgist")
         No11.append(isikukood)
+        ars(No11)
     else:
         print("11 sümbilid")
 
         if isikukood.isdigit()==False:
             print("Isikukood peab sisaldama ainult numbreid")
             NoNum.append(isikukood)
+            ars(NoNum)
         else:
             print("Moodustatud numbritest")
 
@@ -24,6 +27,7 @@ while True:
             if arv=="Viga":
                 print("Üheteistkümnes täht on vale")
                 NoKn.append(isikukood)
+                ars(NoKn)
             else:
                 print(arv)
 
@@ -31,6 +35,7 @@ while True:
                 if sugu=="Viga":
                     print("Esimene täht ei ole õige")
                     NoSug.append(isikukood)
+                    ars(NoSug)
                 else:
                     print(sugu)
 
@@ -38,6 +43,7 @@ while True:
                     if sun=="Viga":
                         print("Viga teise ja seitsmenda tähe vahel")
                         NoSun.append(isikukood)
+                        ars(NoSun)
                     else:
                         print(sun)
 
@@ -45,12 +51,14 @@ while True:
                         if haigla=="Viga":
                             print("Viga kaheksanda ja kümnenda tähe vahel")
                             NoHai.append(isikukood)
+                            ars(NoHai)
                         else:
                             print(haigla)
                             ikood(isikukood)
                             TrueIsikukood.append(isikukood)
+                            TrueI=arg(TrueIsikukood)
                             print()
-    print("Õige isikukoodid - ",TrueIsikukood)
+    print("Õige isikukoodid - ",TrueI)
     print("Isikukood, kus märkide arv on vale - ",No11)
     print("Isikukood, kus see ei koosne ainult numbritest - ",NoNum)
     print("Isikukood vale kontrollnumbriga - ",NoKn)
